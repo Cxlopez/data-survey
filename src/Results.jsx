b971ff6e2095c561f204e88441153c856e5f2a28
Mikko De Los Reyes <mikkode212@gmail.com>
2024-03-22 17:23:54 -0400

    calculate and display stage depending on client average

--- src/Results.jsx
+++ src/Results.jsx
@@ -2,17 +2,28 @@ import React from 'react';
 
 function Results({ overallAverage, industryAverages }) {
   const industryAverage = Object.values(industryAverages).reduce((acc, val) => acc + val, 0) / Object.keys(industryAverages).length;
-
-  console.log(industryAverages,"HERE")
+  const stage = calculateStage(overallAverage, industryAverage);
 
   return (
     <div>
-      Results:
+      <p>Results:</p>
       <p>Your overall average is: {overallAverage}</p>
       <p>The industry average is: {industryAverage}</p>
+      <p>You are in the {stage} stage.</p>
     </div>
   );
 }
 
+function calculateStage(overallAverage, industryAverage) {
+  const difference = overallAverage - industryAverage;
+
+  if (difference >= 0.5) {
+    return 'Emerging';
+  } else if (difference >= 0) {
+    return 'Developing';
+  } else {
+    return 'Optimus Prime';
+  }
+}
 
 export default Results;
