ec0d1f3ada8d12bb201d29fed64b28e130cbfee8
Mikko De Los Reyes <mikkode212@gmail.com>
2024-03-22 15:45:43 -0400

    code formatting

--- src/TermsOfUse.jsx
+++ src/TermsOfUse.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./TermsOfUse.css";
-import Terms from './content/Terms.jsx';
-
+import Terms from "./content/Terms";
 
 function TermsOfUse({ onClose }) {
   return (
@@ -9,10 +8,12 @@ function TermsOfUse({ onClose }) {
       <div className="modal-content">
         <div className="terms-header">
           <h2>Terms of Use</h2>
-          <button className="close-btn" onClick={onClose}>Back to Assessment</button>
+          <button className="close-btn" onClick={onClose}>
+            Back to Assessment
+          </button>
         </div>
         <p className="terms-content">
-        <Terms/>
+          <Terms />
         </p>
       </div>
     </div>
