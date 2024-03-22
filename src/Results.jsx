231d6b8105dbc565fe78eb6a06f054e4ba0c6300
Mikko De Los Reyes <mikkode212@gmail.com>
2024-03-22 17:00:10 -0400

    calculated and rendered overall average of industry averages

--- src/Results.jsx
+++ src/Results.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
-function Results({ overallAverage }) {
+function Results({ overallAverage, industryAverages }) {
+  const industryAverage = Object.values(industryAverages).reduce((acc, val) => acc + val, 0) / Object.keys(industryAverages).length;
+
+  console.log(industryAverages,"HERE")
 
   return (
     <div>
       Results:
       <p>Your overall average is: {overallAverage}</p>
+      <p>The industry average is: {industryAverage}</p>
     </div>
   );
 }
