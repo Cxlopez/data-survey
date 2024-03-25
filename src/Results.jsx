25d70d56c425d9272377c1d59f273f42c9a45889
Mikko De Los Reyes <mikkode212@gmail.com>
2024-03-25 14:20:45 -0400

    styled the results component

--- src/Results.jsx
+++ src/Results.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import './Results.css';
 
 function Results({ overallAverage, industryAverages }) {
   const industryAverage = Object.values(industryAverages).reduce((acc, val) => acc + val, 0) / Object.keys(industryAverages).length;
   const stage = calculateStage(overallAverage, industryAverage);
 
   return (
-    <div>
-      <p>Results:</p>
-      <p>Your overall average is: {overallAverage}</p>
-      <p>The industry average is: {industryAverage}</p>
-      <p>You are in the {stage} stage.</p>
+    <div className="results-container">
+      <p className="results-content">Results:
+      <br></br>
+      Your overall average is: {overallAverage}
+      <br></br>
+      The industry average is: {industryAverage.toFixed(2)}
+      <br></br>
+      You are in the {stage} stage.</p>
     </div>
   );
 }
