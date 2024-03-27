d34a4ed72bb9196badb273891ee311c2099e5fbf
Mikko De Los Reyes <mikkode212@gmail.com>
2024-03-27 12:57:03 -0400

    added overview content and styled

--- src/ResultsHeader.jsx
+++ src/ResultsHeader.jsx
@@ -4,7 +4,7 @@ import './ResultsHeader.css'
 
 function ResultsHeader() {
   return (
-    <div className="header">
+    <div>
       <div className="header-container">
       <img className="header-logo" alt="logo" src={GeoLogo}></img>
       <div>
@@ -12,8 +12,10 @@ function ResultsHeader() {
       </div>
       </div>
       <div className="header-divider"></div>
-      <div className="company-name">
-        <p>[Company Name]</p>
+      <div className="assessment-overview">
+        <p className="company-name">[Company Name]</p>
+        <h1 className="header">Digital Assessment Transformation Results</h1>
+        <p className="overview">This report evaluates your digital performace in eight crucial areas, comparing it to industry benchmarks for success in today's digital age. It also highlights key priorities and areas to concentrate on as you begin your journey of digital evolution and competitiveness.</p>
       </div>
     </div>
   )
