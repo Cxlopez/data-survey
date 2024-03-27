7f8b0e50cdb3cc9f321e6f0bc1acd1a33a09b3a3
Mikko De Los Reyes <mikkode212@gmail.com>
2024-03-27 12:26:08 -0400

    replicated top of header component

--- src/ResultsHeader.jsx
+++ src/ResultsHeader.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import GeoLogo from "./assets/2024-logo-landscape-red-dark.png"
+import './ResultsHeader.css'
 
 function ResultsHeader() {
   return (
     <div className="header">
-      <div>
+      <div className="header-container">
       <img className="header-logo" alt="logo" src={GeoLogo}></img>
-      </div>
       <div>
-
+        <p>www.geodigitalpartners.com</p>
+      </div>
+      </div>
+      <div className="header-divider"></div>
+      <div className="company-name">
+        <p>[Company Name]</p>
       </div>
     </div>
   )
